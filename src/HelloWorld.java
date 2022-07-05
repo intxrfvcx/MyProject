7700158156ff9d8f9c1fce931292441854636758
interface <interfacehunter@yandex.ru>
2022-07-05 17:51:12 +0300

    commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        final int VALUE = 5;
+
+        float x = VALUE;
+        float y = (2*x + 5*x) / 4*x;
+        System.out.println(y);
+    }
+}
