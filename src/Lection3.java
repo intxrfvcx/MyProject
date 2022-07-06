0eaf09bee5e8d05a1c33927302bdf2a312ece722
interface <interfacehunter@yandex.ru>
2022-07-06 13:27:04 +0300

    Lection 2

--- src/Lection3.java
+++ src/Lection3.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
 /*
+Лабораторная работа_1_Арифметика_Конструкции (if, if/else, switch)
 
     Введите первое число: 10 <enter>
     Введите второе число: 15 <enter>
     Введите третье число: 20 <enter>
-            Сумма чисел = 45*/
+            Сумма чисел = 45        */
 
 public class Lection3 {
     public static void main(String[] args) {
--- src/Lection3_2.java
+++ src/Lection3_2.java
@@ -2,8 +2,24 @@
 Угадывание числа осуществляется путем сравнения числа введенного пользователем с числом заранее установленным в программе.
 Результат сравнения должен быть отображен пользователю. */
 
+import java.util.Scanner;
 public class Lection3_2 {
     public static void main(String[] args) {
 
+        System.out.println("Что выйграть, вам необходимо угадать число в диапазоне от 1 до 20");
+        int min = 1;
+        int max = 20;
+        int UserNumber;
+        int UnknownNumber = min + (int)(Math.random() * ((max - min) + 1));
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.print("Введите ваше число: ");
+            UserNumber = input.nextInt();
+            if (UserNumber > UnknownNumber)
+                System.out.println("Моё число меньше.");
+            else if (UserNumber < UnknownNumber) System.out.println("Моё число больше.");
+            else System.out.println("Вы угадали!");
+        } while (UserNumber != UnknownNumber);
     }
 }
