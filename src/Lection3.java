b001bac4c344c90338170064d0b940d7f673707c
interface <interfacehunter@yandex.ru>
2022-07-06 11:10:12 +0300

    Lection 3

--- src/Lection3.java
+++ src/Lection3.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
 
+                    /*          Задание
+                    //Введите первое число: 10 <enter>
+                    //Введите второе число: 15 <enter>
+                    //Введите третье число: 20 <enter>
+                            Сумма чисел = 45*/
+
 public class Lection3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
