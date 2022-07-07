3001924418c14486e64e976cbc36e23904c3f992
interface <interfacehunter@yandex.ru>
2022-07-07 10:34:22 +0300

    Lection 4

---
+++ src/Lection4.java
@@ -0,0 +1,11 @@
+ /* Лабораторная работа 4_Массивы_Циклы
+ Задание: используя цикл for необходимо вывести на экран таблицу умножения на 7. */
+
+public class Lection4 {
+    public static void main(String[] args) {
+
+        for(int i=1; i<=10; i++){
+            System.out.println(i + "*"+ 7 + "=" + i*7);
+        }
+    }
+}
