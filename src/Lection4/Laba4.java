8f4d2c7681cc8803cf0b0811f19bb5bca732b59e
interface <interfacehunter@yandex.ru>
2022-07-10 13:58:14 +0300

    Лабораторная работа 4_Массивы_Циклы Шишкалов Д.Н.

--- src/Lection4/Laba4.java
+++ src/Lection4/Laba4.java
@@ -1,7 +1,7 @@
 package Lection4;
 /* Лабораторная работа 4_Массивы_Циклы
  Задание: используя цикл for необходимо вывести на экран таблицу умножения на 7. */
-
+//
 public class Laba4 {
     public static void main(String[] args) {
 
