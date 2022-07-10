e82dc6d3e9715fa9fb8aaf02d7baee343a55df24
interface <interfacehunter@yandex.ru>
2022-07-10 14:04:57 +0300

    Лабораторная работа_1_Арифметика_Конструкции Шишкалов Д.Н.

--- src/Lection3/Laba1.java
+++ src/Lection3/Laba1.java
@@ -2,7 +2,7 @@ package Lection3;
 import java.util.Scanner;
 /*
 Лабораторная работа_1_Арифметика_Конструкции (if, if/else, switch)
-//
+
     Введите первое число: 10 <enter>
     Введите второе число: 15 <enter>
     Введите третье число: 20 <enter>
